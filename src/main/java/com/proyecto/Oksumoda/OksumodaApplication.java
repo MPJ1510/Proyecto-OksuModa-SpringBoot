97fef38bb888077b6d77f05047c598a7817f2e92
MPJ1510 <maikelmacias01@gmail.com>
2025-11-19 21:36:52 -0500

    Creación de Carpetas y Archivos HTML, CSS y Imágenes

---
+++ src/main/java/com/proyecto/Oksumoda/OksumodaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.Oksumoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OksumodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OksumodaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/Oksumoda/OksumodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.Oksumoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OksumodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
